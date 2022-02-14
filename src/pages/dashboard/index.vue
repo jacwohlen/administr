97b4b648e8c8df3418f6f2f29924de745e1548b7
Andreas Schmid <andreas.schmid.as3@roche.com>
2022-02-14 13:01:09 +0100

    Navigate through dates, showing trainings as list instead of cards

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,22 +1,44 @@
 <template>
   <div>
-    <h1><v-icon>mdi-calendar-check</v-icon> {{ title }}</h1>
-    <v-card v-for="(t, idx) in trainings" :key="idx" class="mb-3">
-      <v-card-title>
-        {{ t.title }}
-        <v-subheader>(id: {{ t.id }})</v-subheader>
-      </v-card-title>
-      <v-card-subtitle>
-        {{ t.weekday }}, {{ t.dateFrom }} - {{ t.dateTo }}
-        <v-chip small>{{ t.section }}</v-chip>
-      </v-card-subtitle>
-      <v-card-actions>
-        <v-btn color="primary" text :to="'dashboard/' + t.id + '/' + todayStr">
-          <v-icon left> mdi-format-list-checks </v-icon>
-          Present List
+    <h1 align="center" class="text-h5" @click="goToday">{{ title }}</h1>
+    <v-row>
+      <v-col align="right">
+        <v-btn text @click="goYesterday"
+          ><v-icon>mdi-arrow-left</v-icon> Yesterday</v-btn
+        >
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col align="left">
+        <v-btn text @click="goTomorrow">
+          Tomorrow
+          <v-icon>mdi-arrow-right</v-icon>
         </v-btn>
-      </v-card-actions>
-    </v-card>
+      </v-col>
+    </v-row>
+    <v-list>
+      <v-list-item v-for="(t, idx) in trainings" :key="idx" class="mb-3">
+        <v-list-item-content>
+          <v-list-item-title v-if="t.title">
+            {{ t.title }}
+          </v-list-item-title>
+          <v-list-item-title v-else> (id: {{ t.id }}) </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ t.weekday }}, {{ t.dateFrom }} - {{ t.dateTo }}
+            <v-chip small>{{ t.section }}</v-chip>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn
+            color="primary"
+            text
+            :to="'dashboard/' + t.id + '/' + todayStr"
+          >
+            <v-icon left> mdi-format-list-checks </v-icon>
+            <span class="hidden-sm-and-down">Present List</span>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
@@ -34,26 +56,48 @@ import { administraStore } from '~/store'
     await administraStore.init()
   },
 })
-export default class CategoriesPage extends Vue {
+export default class extends Vue {
   async mounted() {
     // client side
     await administraStore.init()
   }
 
+  // FIX: why is moment becoming a string in title()
+  // it should be an object of type moment as defined here
+  // that's why moment(this.date) in all the functions
+  date: moment.Moment = moment()
+
   get title() {
-    return this.today.format('ddd, LL')
+    const d = moment(this.date)
+    return d.format('ddd, LL')
   }
 
   get todayStr() {
-    return this.today.format('yyyy-MM-DD')
+    const d = moment(this.date)
+    return d.format('yyyy-MM-DD')
   }
 
-  get today() {
-    return moment()
+  get weekday() {
+    const d = moment(this.date)
+    return d.format('DDD')
   }
 
   get trainings() {
     return administraStore.trainings
+  }
+
+  goTomorrow() {
+    const d = moment(this.date)
+    this.date = d.add(1, 'days')
+  }
+
+  goYesterday() {
+    const d = moment(this.date)
+    this.date = d.subtract(1, 'days')
+  }
+
+  goToday() {
+    this.date = moment()
   }
 }
 </script>
