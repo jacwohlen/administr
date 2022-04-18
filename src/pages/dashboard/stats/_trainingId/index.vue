d652749e715fe42737559bbb35e33a800f6206bd
Andreas Schmid <ikeark@gmail.com>
2022-04-18 23:03:02 +0200

    WIP

--- src/pages/dashboard/stats/_trainingId/index.vue
+++ src/pages/dashboard/stats/_trainingId/index.vue
@@ -1,25 +1,47 @@
 <template>
   <div>
-    <h1>{{ userId }} - {{ date }}</h1>
-    <v-data-table dense :headers="dates" :items="items" :item-key="name">
+    <h1>{{ training.name }}</h1>
+    <v-data-table
+      :items="dates"
+      :headers="items"
+      disable-sort
+      hide-default-footer
+      dense
+    >
     </v-data-table>
+
+    <h2>Table 2</h2>
+    <table>
+      <thead>
+        <th>Names</th>
+        <th v-for="d in dates" :key="d.value">{{ d.text }}</th>
+      </thead>
+      <tr v-for="m in members" :key="m.id">
+        <td>{{ m.lastname }} {{ m.firstname }}</td>
+        <td v-for="d in dates" :key="d.value">X</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-import { administraStore } from '~/store'
 import moment from 'moment'
+
+import { administraStore } from '~/store'
 
 @Component({
   layout: 'DashboardLayout',
   async asyncData({ params }) {
     // await administraStore.init()
-    await administraStore.initStats({
-      trainingId: params.userId,
+    await administraStore.initTraining({
+      trainingId: params.trainingId,
     })
-    return { userId: params.userId }
+    await administraStore.initStats({
+      trainingId: params.trainingId,
+    })
+    return { trainingId: params.trainingId }
   },
 })
 export default class extends Vue {
@@ -34,15 +56,15 @@ export default class extends Vue {
     if (this.date.date() > 7) {
       this.date = this.date.isoWeekday(-6)
     }
-    this.dates.push({ text: 'Judoka', value: 'name' })
+    this.dates.push({ text: 'Name', value: 'name' })
     while (this.dates.length < 52) {
       this.dates.push({
-        text: moment(this.date).format('DD.MM.YYYY'),
+        text: moment(this.date).week(), // format('DD.MM.YYYY'),
         value: moment(this.date).format('YYYY-MM-DD'),
+        class: 'rotate',
       })
       this.date = this.date.add(7, 'days')
     }
-    console.log(this.dates)
   }
 
   get items() {
@@ -67,11 +89,25 @@ export default class extends Vue {
       },
     ]
   }
+
+  get members() {
+    return [
+      {
+        id: '1',
+        lastname: 'Penelope',
+        firstname: 'Penelope',
+      },
+      {
+        id: '2',
+        lastname: 'Franz',
+        firstname: 'Penelope',
+      },
+      {
+        id: '3',
+        lastname: 'Enis',
+        firstname: 'Penelope',
+      },
+    ]
+  }
 }
 </script>
-
-<style>
-.rotate {
-  transform: rotate(-90deg);
-}
-</style>
