e3c0ca26665610068a51a83d60ccc46db38b32f0
Andreas Schmid <andreas.schmid.as3@roche.com>
2022-08-21 19:00:13 +0200

    Performance: Remove unused CSS with purgecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+import PurgecssPlugin from 'purgecss-webpack-plugin'
+import glob from 'glob-all'
+import path from 'path'
+
 export default {
   srcDir: 'src/',
   target: 'static',
@@ -9,6 +13,20 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     extractCSS: true,
+    extend(config, { isDev, isClient }) {
+      if (!isDev && isClient) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './src/pages/**/*.vue'),
+              path.join(__dirname, './src/layouts/**/*.vue'),
+              path.join(__dirname, './src/components/**/*.vue'),
+            ]),
+            whitelist: ['html', 'body'],
+          })
+        )
+      }
+    },
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
